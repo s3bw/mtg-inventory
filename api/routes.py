efd691ebb00b9b09c5d395eff45cb323291e43dc
Sebastien Williams-Wynn <s.williamswynn.mail@gmail.com>
2020-06-11 02:06:48 +0100

    Add API routes module

---
+++ api/routes.py
@@ -0,0 +1,86 @@
+import logging
+
+from flask import request
+from flask import g, Blueprint
+from flask_restplus import Api, Resource
+
+from .library import verify_payload
+
+
+routes = Blueprint("route", __name__)
+api = Api(routes)
+
+
+@api.route("/inventory")
+class Inventory(Resource):
+    """Fetch inventory."""
+
+    def get(self):
+        """ This should return everything in
+            my collection.
+        """
+        # Calculate 'InStock' and 'InDeck'
+        # attributes.
+        # InStock = card.quantity - sum_for_all_decks(n of cards)
+        #   this means that if a card is in another deck it can
+        #   not be selected for current deck. (We should still
+        #   return cards with a 0 InStock value).
+        # InDeck = 0 (this depends on the client side and the
+        #   current deck that has been selected.
+        return [
+            # Card,
+            # Card,
+            # Card,
+        ]
+
+    # @verify_payload(Card)
+    def post(self, payload):
+        """Add a card to the collection."""
+        # Return base64'd card name, this will
+        # be used as an ID.
+        return None
+
+
+@api.route("/deck")
+class AllDecks(Resource):
+    """Fetch decks."""
+
+    def post(self):
+        """Create a new deck."""
+        # When creating a new deck the id of
+        # the items within the deck with need
+        # to be added to a card_deck table.
+
+        # Returns a uuid of the new deck
+        pass
+
+    def get(self):
+        """Fetch all decks."""
+
+        return [
+            # Deck,
+            # Deck,
+            # Deck,
+        ]
+
+
+@api.route("/deck/<str:id>")
+class Deck(Resource):
+    """Interact against Deck resource."""
+
+    def get(self, id):
+        """Fetch a deck by it's id."""
+        # Returns 'Deck' object
+        # this will contain a list of items in
+        # the deck and how many of each items
+        # exist in the deck. E.g. (2x of item.id)
+        pass
+
+    def delete(self, id):
+        """Delete a deck by id."""
+        # Once a deck is deleted items that have
+        # been added to this deck will have to
+        # be removed.
+
+        # Returns 'success' once deck is deleted
+        pass
